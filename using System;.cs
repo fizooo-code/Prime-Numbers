31b7575f5971db3cc324499babe7616623d747c3
Faiza0107 <161153360+Faiza0107@users.noreply.github.com>
2025-03-09 11:37:48 +0500

    Update using System;.cs

--- using System;.cs
+++ using System;.cs
@@ -1,35 +1,27 @@
-using System;
 
 class Program
 {
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+        
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
     static void Main()
     {
-        Console.Write("Enter first number: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
 
-        Console.Write("Enter second number: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
-
-        int sum = num1 + num2;
-        int difference = num1 - num2;
-        int product = num1 * num2;
-        double quotient = (num2 != 0) ? (double)num1 / num2 : double.NaN;
-
-        Console.WriteLine("\nResults:");
-        Console.WriteLine($"Sum: {sum}");
-        Console.WriteLine($"Difference: {difference}");
-        Console.WriteLine($"Product: {product}");
-        Console.WriteLine($"Quotient: {quotient}");
-
-        //Console.WriteLine("\n");
-for (int i = 1; i < 10; i++)
-{
-    Console.Write(i + " ");
-}
-
-Console.WriteLine("\n\nEven numbers from");
-for (int i = 2; i < 20; i += 2)
-{
-    Console.Write(i + " ");
-}
+        if (IsPrime(num))
+            Console.WriteLine($"{num} is a prime number.");
+        else
+            Console.WriteLine($"{num} is not a prime number.");
+    }
 }
